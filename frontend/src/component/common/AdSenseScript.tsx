4e4bdb030dd51bf07c57f7116b4764e1734098a3
Hien Cider <tran_minh_hien@cider.systems>
2026-05-18 16:09:38 +0700

    feat: add google adsense + ads.txt

--- frontend/src/component/common/AdSenseScript.tsx
+++ frontend/src/component/common/AdSenseScript.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { ADSENSE_CLIENT, isAdsenseConfigured } from "@/core/adsense";
 
 export default function AdSenseScript() {
@@ -7,12 +6,10 @@ export default function AdSenseScript() {
   }
 
   return (
-    <Script
-      id="google-adsense"
+    <script
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
     />
   );
 }
